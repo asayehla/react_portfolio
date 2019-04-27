f7c1d0f7eb8c8772595c4c26cc185cd548b3bb29
Åsa Palholmen <asa@palholmen.se>
2019-04-27 15:00:49 +0200

    added footer header and hello component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Hello from './components/Hello.jsx';
+import './css/tailwind.css';
 
 function App() {
   return (
-    <>
-    Hello
-    </>
+    <div className="min-h-screen">
+      <div className="flex flex-col w-full fixed pin-l pin-y">
+        <Header />
+        <div className="px-4 pt-4 flex-1 overflow-y-scrol">
+          <Hello />
+        </div>
+        <Footer />
+      </div >
+    </div >
   );
 }
 
